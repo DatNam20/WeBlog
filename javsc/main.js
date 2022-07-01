7702650e564632b5754b514110a7353167183f98
DEEPALI SINGH <deepalisingh15899@gmail.com>
2022-07-01 18:08:22 +0530

    Javascript file connected

---
+++ javsc/main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /* on click event for toggle menu */
+
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
+
+
+
+
+
+});
