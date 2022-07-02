7241132d9a747d779d35d2669dcf4a4a10ce0633
DEEPALI SINGH <deepalisingh15899@gmail.com>
2022-07-02 18:32:30 +0530

    Create Blog Posts Section

--- javsc/main.js
+++ javsc/main.js
@@ -4,14 +4,15 @@ $(document).ready(function(){
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
 
-    /* on click event for toggle menu */
 
+    /* on click event for toggle menu */
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
     })
 
 
-
+    //owl-carousel for blog
+    $('.owl-carousel').owlCarousel();
 
 
 });
