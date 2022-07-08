e94c24b21ba0bf4a1e5f67330d33d64541264bb8
DEEPALI SINGH <deepalisingh15899@gmail.com>
2022-07-08 17:26:22 +0530

    Added 'go to top of page' feature

--- javsc/main.js
+++ javsc/main.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
     $toggleCollapse = $('.toggle-collapse');
 
 
+
     /* on click event for toggle menu */
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
     })
+
 
 
     //owl-carousel for blog
@@ -20,6 +22,15 @@ $(document).ready(function(){
         nav: true,
         navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
     });
+
+
+
+    //scroll up to top
+    $('go-to-top span').click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
 
 
 });
